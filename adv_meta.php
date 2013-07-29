0d91101953579b210c020bfe8d30eab8f7bb1cfb
Shawn Sandy <shawnsandy04@gmail.com>
2013-07-29 00:25:42 -0400

    First Commit

---
+++ adv_meta.php
@@ -0,0 +1,80 @@
+<?php
+
+/**
+ * The file description. *
+ * @package Pico
+ * @subpackage Basejump
+ * @since BJ 1.0
+ * @author Shawn Sandy <shawnsandy04@gmail.com>
+ */
+class Adv_Meta {
+
+    private $meta_values = array(),
+            $content = null,
+            $config = null;
+
+    public function __construct() {
+
+    }
+
+    public function before_load_content(&$file) {
+
+        $this->content = file_get_contents($file);
+
+    }
+
+    public function config_loaded(&$settings) {
+
+        $this->config = $settings;
+
+    }
+
+    public function file_meta(&$meta) {
+
+        $adv_meta = $this->adv_file_meta();
+
+        $meta = array_merge($meta, $adv_meta);
+
+        //var_dump($meta);
+
+        return $meta;
+    }
+
+
+    /**
+     * Grab the file meta here
+     * @return string
+     */
+    private function adv_file_meta() {
+
+        //include the config aand grab some useful values
+        $content = $this->content;
+        $config = $this->config;
+
+//        $adv_config = dirname(__FILE__) . 'adv_meta_config.php';
+//
+//        if (file_exists($adv_config))
+//            include_once $adv_config;
+
+        if (!isset($this->config))
+            $config = array();
+
+        if (isset($config['adv_meta_values']))
+            $this->meta_values = $config['adv_meta_values'];
+
+        $headers = $this->meta_values;
+
+        foreach ($headers as $field => $regex) {
+            if (preg_match('/^[ \t\/*#@]*' . preg_quote($regex, '/') . ':(.*)$/mi', $content, $match) && $match[1]) {
+                $headers[$field] = trim(preg_replace("/\s*(?:\*\/|\?>).*/", '', $match[1]));
+            } else {
+                $headers[$field] = '';
+            }
+        }
+
+        //$this->adv_meta = $headers;
+
+        return $headers;
+    }
+
+}
---
+++ adv_meta_config.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * The file description. *
+ * @package WordPress
+ * @subpackage Basejump
+ * @since BJ 1.0
+ * @author Shawn Sandy <shawnsandy04@gmail.com>
+ */
+
+?>
